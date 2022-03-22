b2d1f57b25199904ce3cb584c8071974aef42574
Talhaaa99 <talha.toufeeq99@gmail.com>
2022-03-22 19:43:57 +0530

    Header done

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="./images/TrollFace.png"></img>
+            <h1 className="header--title">Meme Generator</h1>
+            <h4 className="header--info">React Course - Project 3</h4>
+        </header>
+    );
+}
